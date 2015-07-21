535588e728d5b3d833aeceb355ce44fcd0c855cc
Gedion Woldeselassie <wolde034@x-134-84-3-175.vpn.umn.edu>
2015-07-21 14:24:16 -0500

    MDL- lib/conditionlib: Occasional cron error
    Inhibit division by zero error warnings when rawgrademax and rawgrademin are
    equal. The change does not affect existing functional behavior.

--- availability/condition/grade/classes/condition.php
+++ availability/condition/grade/classes/condition.php
@@ -228,7 +228,9 @@ class condition extends \core_availability\condition {
                         WHERE
                             gi.courseid = ?', array($userid, $courseid));
                 foreach ($rs as $record) {
-                    if (is_null($record->finalgrade)) {
+                    # This function produces division by zero error warnings when rawgrademax and rawgrademin
+                    # are equal. Below change does not affect function behavior, just avoids the warning.
+                    if (is_null($record->finalgrade) || $record->rawgrademax == $record->rawgrademin) {
                         // No grade = false.
                         $cachedgrades[$record->id] = false;
                     } else {
